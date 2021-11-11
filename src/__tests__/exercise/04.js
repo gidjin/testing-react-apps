524c6c73c40f3193f36b0f20de0d64ffab3f9f6e
John Gedeon <john@truss.works>
2021-11-10 22:11:49 -0800

    Complete 4 Form Testing 4

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -7,10 +7,11 @@ import userEvent from '@testing-library/user-event'
 import faker from 'faker'
 import Login from '../../components/login'
 
-const buildLoginForm = () => {
+const buildLoginForm = (params) => {
   return {
     username: faker.internet.userName(),
-    password: faker.internet.password()
+    password: faker.internet.password(),
+    ...params,
   }
 }
 
@@ -24,6 +25,28 @@ test('submitting the form calls onSubmit with username and password', () => {
   // 🐨 use userEvent.type to change the username and password fields to
   //    whatever you want
   const { username, password } = buildLoginForm()
+  userEvent.type(usernameInput, username)
+  userEvent.type(passwordInput, password)
+  // 🐨 click on the button with the text "Submit"
+  const submit = screen.getByRole("button")
+  userEvent.click(submit)
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username,
+    password,
+  })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+})
+
+test('submitting the form calls onSubmit with username and specific password', () => {
+  const handleSubmit = jest.fn()
+  // 🐨 render the login with your handleSubmit function as the onSubmit prop
+  render(<Login onSubmit={handleSubmit} />)
+  // 🐨 get the username and password fields via `getByLabelText`
+  const usernameInput = screen.getByLabelText(/username/i)
+  const passwordInput = screen.getByLabelText(/password/i)
+  // 🐨 use userEvent.type to change the username and password fields to
+  //    whatever you want
+  const { username, password } = buildLoginForm({ password: "abc" })
   userEvent.type(usernameInput, username)
   userEvent.type(passwordInput, password)
   // 🐨 click on the button with the text "Submit"
