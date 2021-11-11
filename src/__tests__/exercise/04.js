0f38ffaf7b5cfd80751a2f7f64c93d6c87a0a13e
John Gedeon <john@truss.works>
2021-11-10 22:17:02 -0800

    Complete 4 Form Testing 5

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,16 +4,15 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import faker from 'faker'
+import { build, fake } from '@jackfranklin/test-data-bot'
 import Login from '../../components/login'
 
-const buildLoginForm = (params) => {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...params,
-  }
-}
+const buildLoginForm = build('LoginForm', {
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
