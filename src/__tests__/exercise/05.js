844170c1940f7f8c695203c5cf23b910c2123788
John Gedeon <john@truss.works>
2021-11-19 16:25:48 -0800

    Complete 5 Mocking HTTP Request 2 + Extra Credit 1

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -8,6 +8,7 @@ import {build, fake} from '@jackfranklin/test-data-bot'
 import {rest} from 'msw'
 import {setupServer} from 'msw/node'
 import Login from '../../components/login-submission'
+import {handlers} from 'test/server-handlers'
 const buildLoginForm = build({
   fields: {
     username: fake(f => f.internet.userName()),
@@ -15,18 +16,9 @@ const buildLoginForm = build({
   },
 })
 
-const server = setupServer(
-  rest.post(
-   'https://auth-provider.example.com/api/login',
-   async (req, res, ctx) => {
-     return res(ctx.json({username: req.body.username}))
-   },
- )
-)
+const server = setupServer(...handlers)
 
-// 🐨 before all the tests, start the server with `server.listen()`
 beforeAll(() => server.listen())
-// 🐨 after all the tests, stop the server with `server.close()`
 afterAll(() => server.close())
 
 test(`logging in displays the user's username`, async () => {
@@ -37,14 +29,7 @@ test(`logging in displays the user's username`, async () => {
   userEvent.type(screen.getByLabelText(/password/i), password)
   userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
-  // as soon as the user hits submit, we render a spinner to the screen. That
-  // spinner has an aria-label of "loading" for accessibility purposes, so
-  // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
-  // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
   await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
 
-  // once the login is successful, then the loading spinner disappears and
-  // we render the username.
-  // 🐨 assert that the username is on the screen
   expect(screen.getByText(username)).toBeInTheDocument()
 })
